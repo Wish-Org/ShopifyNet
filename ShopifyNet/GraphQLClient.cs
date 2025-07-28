febb7dc4008a996e65a6eb3666b4d9bfc6cd9a56
Clement <clement.gutel@gmail.com>
2025-07-28 22:49:13 +1000

    Update GraphQLSharp package to version 2.9.4 and refactor GraphQLClient to include request priority and interceptor logic

--- ShopifyNet/GraphQLClient.cs
+++ ShopifyNet/GraphQLClient.cs
@@ -1,8 +1,24 @@
+using GraphQLSharp;
+
 namespace ShopifyNet;
 
 public partial class GraphQLClient
 {
-    protected override void ValidateOptions(AdminClientOptions defaultOptions, AdminClientOptions options)
+    private readonly Func<int> _getRequestPriority;
+    private readonly IInterceptor _tokenBucketWithRetryInterceptor;
+
+    public GraphQLClient(ShopifyClientOptions defaultOptions = null, Func<int> getRequestPriority = null)
+        : this(defaultOptions)
+    {
+        _getRequestPriority = getRequestPriority;
+        _tokenBucketWithRetryInterceptor = new ChainedInterceptor(new TokenBucketInterceptor(_getRequestPriority),
+                                                                  new RetryInterceptor());
+    }
+
+
+    protected override IInterceptor DefaultInterceptor => _tokenBucketWithRetryInterceptor;
+
+    protected override void ValidateOptions(ShopifyClientOptions defaultOptions, ShopifyClientOptions options)
     {
         _ = defaultOptions?.MyShopifyDomain ?? options?.MyShopifyDomain ?? throw new ArgumentNullException(nameof(options.MyShopifyDomain));
         _ = defaultOptions?.AccessToken ?? options?.AccessToken ?? throw new ArgumentNullException(nameof(options.AccessToken));
