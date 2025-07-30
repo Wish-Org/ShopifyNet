921f455bfcc95fe2517a12e042021eb804edefec
Clement <clement.gutel@gmail.com>
2025-07-30 22:33:34 +1000

    Add initial unit tests for TokenBucketInterceptor functionality

---
+++ ShopifyNet.Tests/TokenBucket/TokenBucketInterceptorTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShopifyNet.Tests;
+
+[TestClass]
+public class TokenBucketInterceptorTests
+{
+    [TestInitialize]
+    public void Initialize()
+    {
+    }
+
+    [TestMethod]
+    public void BucketRefillsUpToMax()
+    {
+    }
+}
