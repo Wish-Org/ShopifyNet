f022e4ad496762ea0599c239d64e4386f079c94e
Clement <clement.gutel@gmail.com>
2025-08-07 12:59:13 +1000

    Update ShopifyClient constructor and bump project version to 2.2.0

--- ShopifyNet/ShopifyClient.cs
+++ ShopifyNet/ShopifyClient.cs
@@ -5,6 +5,14 @@ namespace ShopifyNet;
 
 public partial class ShopifyClient
 {
+    public ShopifyClient(string myShopifyDomain,
+                        string accessToken,
+                        string apiVersion = ShopifyClientOptions.DEFAULT_API_VERSION,
+                        bool useSmartInterceptor = true)
+                        : this(new ShopifyClientOptions(myShopifyDomain, accessToken, apiVersion, useSmartInterceptor))
+    {
+    }
+
     public Task<GraphQLResponse<Mutation>> MutationAsync(string query, int? cost = null, CancellationToken cancellationToken = default)
     {
         var request = new ShopifyGraphQLRequest
